cd471f686596fcdcb883c671d23f8a1421e4a545
lemoeJuice <1263471878@qq.com>
2024-12-17 17:52:23 +0800

    修改序列码实现方式，同时引入ResultStore类以支持api操作的检验

--- lolibot/__init__.py
+++ lolibot/__init__.py
@@ -1,4 +1,5 @@
-import sys
+from typing import Dict, Any
+import asyncio
 
 try:
     import ujson as json
@@ -9,17 +10,41 @@ from quart import Quart, websocket
 
 
 class _SequenceGenerator:
-    _seq = 1
+    _seq = -1
 
     @classmethod
     def next(cls) -> int:
-        s = cls._seq
-        cls._seq = (cls._seq + 1) % sys.maxsize
-        return s
+        cls._seq = (cls._seq + 1) % 2147483648
+        return cls._seq
+
+
+class ResultStore:
+    _futures: Dict[int, asyncio.Future] = {}
+
+    @classmethod
+    def add(cls, result: Dict[str, Any]):
+        if isinstance(result.get('echo'), int):
+            future = cls._futures.get(result['echo'])
+            if future:
+                future.set_result(result)
+
+    @classmethod
+    async def fetch(cls, seq: int, timeout_sec: float) -> Dict[str, Any]:
+        future = asyncio.get_event_loop().create_future()
+        cls._futures[seq] = future
+        try:
+            return await asyncio.wait_for(future, timeout_sec)
+        except asyncio.TimeoutError:
+            # haven't received any result until timeout,
+            # we consider this API call failed with a network error.
+            raise  # NetworkError('WebSocket API call timeout')
+        finally:
+            # don't forget to remove the future object
+            del cls._futures[seq]
 
 
 class Message:
-    def __init__(self, is_group: bool, obj_id: int, content):
+    def __init__(self, is_group: bool, obj_id: int, content: Dict[str, Any]):
         self.is_group = is_group
         self.id = obj_id
         self.content = content
@@ -31,26 +56,31 @@ class Message:
             return {'message_type': 'private', 'user_id': self.id, 'message': self.content}
 
     async def send(self):
-        seq = _SequenceGenerator.next()
-        await websocket.send(json.dumps({'action': 'send_msg', 'params': self.to_json(), 'echo': {'seq': seq}}))
+        seq: int = _SequenceGenerator.next()
+        await websocket.send(json.dumps({'action': 'send_msg', 'params': self.to_json(), 'echo': seq}))
 
 
 async def on_wsr_connect():
-    pass
+    print('Bot Client Connected.')
 
 
-async def _handle_meta(payload):
-    print(f'Meta Event: {payload}')
-    if payload.get('meta_event_type') == 'lifecycle' and payload.get('sub_type') == 'connect':
+async def _handle_meta(payload: Dict[str, Any]):
+    # print(f'Meta Event: {payload}')
+    if payload.get('meta_event_type') == 'heartbeat':  # 需要处理心跳事件
+        interval = payload.get('interval')
+        status = payload.get('status')
+        print(f'Heartbeat detected by interval {interval}ms : {status}')
+    elif payload.get('meta_event_type') == 'lifecycle' and payload.get('sub_type') == 'connect':
         await on_wsr_connect()
-    # 需要处理心跳事件
+    else:
+        print(f'Unknown meta_event: {payload}')
 
 
-async def _handle_message(payload):
+async def _handle_message(payload: Dict[str, Any]):
     print(f'Message: {payload}')
 
 
-async def _handle_api(payload):
+async def _handle_api(payload: Dict[str, Any]):
     print(f'Api: {payload}')
 
 
@@ -62,7 +92,7 @@ async def _handle_wsr() -> None:
                 await _handle_meta(payload)
             elif post_type == 'message':
                 await _handle_message(payload)
-            else:
+            else:  # 会不会有其他情况
                 await _handle_api(payload)
 
 
